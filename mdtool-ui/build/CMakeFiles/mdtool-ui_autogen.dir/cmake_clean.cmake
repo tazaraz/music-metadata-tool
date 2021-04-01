3e29dfc02508a19073b24dd5d619353111e31dce
Emo Maat <emosbm@gmail.com>
2021-04-01 11:44:18 +0200

    Added ignore file

--- mdtool-ui/build/CMakeFiles/mdtool-ui_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/mdtool-ui_autogen"
-  "mdtool-ui_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/mdtool-ui_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- mdtool/build/CMakeFiles/mdtool.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for mdtool.
-# This may be replaced when dependencies are built.
